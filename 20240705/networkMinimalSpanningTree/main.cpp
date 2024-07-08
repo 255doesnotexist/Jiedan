e431d029dbeb8d14e310d590e568715c15aef0dc
255doesnotexist <chenyejin2004@stumail.ysu.edu.cn>
2024-07-08 16:15:14 +0800

    update

--- 20240705/networkMinimalSpanningTree/main.cpp
+++ 20240705/networkMinimalSpanningTree/main.cpp
@@ -3,143 +3,152 @@
 #include <vector>
 #include <algorithm>
 #include <limits>
+#include <string>
 
 using namespace std;
 
 struct Computer {
-	string name;
-	string location;
+    string name;
+    string location;
 };
 
 struct Edge {
-	int from, to, weight;
+    int from, to, weight;
 };
 
 vector<Computer> computers;
-vector<vector<int>> network;
+vector<vector<int> > network;
 
 void readComputerInfo(const string& filename) {
-	ifstream file(filename);
-	int n;
-	file >> n;
-	computers.resize(n);
-	for (int i = 0; i < n; i++) {
-		file >> computers[i].name >> computers[i].location;
-	}
+    ifstream file(filename.c_str());
+    int n;
+    file >> n;
+    computers.resize(n);
+    for (int i = 0; i < n; i++) {
+        file >> computers[i].name >> computers[i].location;
+    }
 }
 
 void readNetworkInfo(const string& filename) {
-	ifstream file(filename);
-	int n = computers.size();
-	network.resize(n, vector<int>(n));
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			file >> network[i][j];
-		}
-	}
+    ifstream file(filename.c_str());
+    int n = computers.size();
+    network.resize(n, vector<int>(n));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            file >> network[i][j];
+        }
+    }
 }
 
 vector<Edge> prim() {
-	int n = network.size();
-	vector<bool> visited(n, false);
-	vector<Edge> result;
-	visited[0] = true;
-	
-	for (int i = 0; i < n - 1; i++) {
-		Edge minEdge = {-1, -1, numeric_limits<int>::max()};
-		for (int j = 0; j < n; j++) {
-			if (visited[j]) {
-				for (int k = 0; k < n; k++) {
-					if (!visited[k] && network[j][k] > 0 && network[j][k] < minEdge.weight) {
-						minEdge = {j, k, network[j][k]};
-					}
-				}
-			}
-		}
-		visited[minEdge.to] = true;
-		result.push_back(minEdge);
-	}
-	return result;
+    int n = network.size();
+    vector<bool> visited(n, false);
+    vector<Edge> result;
+    visited[0] = true;
+    
+    for (int i = 0; i < n - 1; i++) {
+        Edge minEdge = {-1, -1, numeric_limits<int>::max()};
+        for (int j = 0; j < n; j++) {
+            if (visited[j]) {
+                for (int k = 0; k < n; k++) {
+                    if (!visited[k] && network[j][k] > 0 && network[j][k] < minEdge.weight) {
+                        minEdge = (Edge){j, k, network[j][k]};
+                    }
+                }
+            }
+        }
+        visited[minEdge.to] = true;
+        result.push_back(minEdge);
+    }
+    return result;
+}
+
+int find(vector<int>& parent, int x) {
+    if (parent[x] != x) {
+        parent[x] = find(parent, parent[x]);
+    }
+    return parent[x];
+}
+
+void unite(vector<int>& parent, int x, int y) {
+    parent[find(parent, x)] = find(parent, y);
 }
 
 vector<Edge> kruskal() {
-	int n = network.size();
-	vector<Edge> edges;
-	for (int i = 0; i < n; i++) {
-		for (int j = i + 1; j < n; j++) {
-			if (network[i][j] > 0) {
-				edges.push_back({i, j, network[i][j]});
-			}
-		}
-	}
-	sort(edges.begin(), edges.end(), [](const Edge& a, const Edge& b) {
-		return a.weight < b.weight;
-	});
-	
-	vector<int> parent(n);
-	for (int i = 0; i < n; i++) parent[i] = i;
-	
-	std::function<int(int)> find = [&](int x) {
-		if (parent[x] != x) parent[x] = find(parent[x]);
-		return parent[x];
-	};
-	
-	auto unite = [&](int x, int y) {
-		parent[find(x)] = find(y);
-	};
-	
-	vector<Edge> result;
-	for (const auto& edge : edges) {
-		if (find(edge.from) != find(edge.to)) {
-			unite(edge.from, edge.to);
-			result.push_back(edge);
-		}
-	}
-	return result;
+    int n = network.size();
+    vector<Edge> edges;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (network[i][j] > 0) {
+                edges.push_back((Edge){i, j, network[i][j]});
+            }
+        }
+    }
+    sort(edges.begin(), edges.end(), [](const Edge& a, const Edge& b) {
+        return a.weight < b.weight;
+    });
+    
+    vector<int> parent(n);
+    for (int i = 0; i < n; i++) parent[i] = i;
+    
+    vector<Edge> result;
+    for (size_t i = 0; i < edges.size(); i++) {
+        const Edge& edge = edges[i];
+        if (find(parent, edge.from) != find(parent, edge.to)) {
+            unite(parent, edge.from, edge.to);
+            result.push_back(edge);
+        }
+    }
+    return result;
 }
 
 void writeResult(const string& filename) {
-	ofstream file(filename);
-	
-	// å†™å…¥è®¡ç®—æœºä¿¡æ¯
-	file << computers.size() << endl;
-	for (const auto& comp : computers) {
-		file << comp.name << " " << comp.location << endl;
-	}
-	
-	// å†™å…¥åŽŸå§‹ç½‘ç»œä¿¡æ¯
-	for (const auto& row : network) {
-		for (int weight : row) {
-			file << weight << " ";
-		}
-		file << endl;
-	}
-	
-	// å†™å…¥Primç®—æ³•ç»“æžœ
-	auto primResult = prim();
-	int primTotalWeight = 0;
-	file << "Prim" << endl;
-	for (const auto& edge : primResult) {
-		file << edge.from << " " << edge.to << " " << edge.weight << endl;
-		primTotalWeight += edge.weight;
-	}
-	file << "PrimTotalWeight " << primTotalWeight << endl;
-	
-	// å†™å…¥Kruskalç®—æ³•ç»“æžœ
-	auto kruskalResult = kruskal();
-	int kruskalTotalWeight = 0;
-	file << "Kruskal" << endl;
-	for (const auto& edge : kruskalResult) {
-		file << edge.from << " " << edge.to << " " << edge.weight << endl;
-		kruskalTotalWeight += edge.weight;
-	}
-	file << "KruskalTotalWeight " << kruskalTotalWeight << endl;
+    ofstream file(filename.c_str());
+    
+    // Ð´Èë¼ÆËã»úÐÅÏ¢
+    file << computers.size() << endl;
+    for (size_t i = 0; i < computers.size(); i++) {
+        const Computer& comp = computers[i];
+        file << comp.name << " " << comp.location << endl;
+    }
+    
+    // Ð´ÈëÔ­Ê¼ÍøÂçÐÅÏ¢
+    for (size_t i = 0; i < network.size(); i++) {
+        const vector<int>& row = network[i];
+        for (size_t j = 0; j < row.size(); j++) {
+            file << row[j] << " ";
+        }
+        file << endl;
+    }
+    
+    // Ð´ÈëPrimËã·¨½á¹û
+    vector<Edge> primResult = prim();
+    int primTotalWeight = 0;
+    file << "Prim" << endl;
+    for (size_t i = 0; i < primResult.size(); i++) {
+        const Edge& edge = primResult[i];
+        file << edge.from << " " << edge.to << " " << edge.weight << endl;
+        primTotalWeight += edge.weight;
+    }
+    file << "PrimTotalWeight " << primTotalWeight << endl;
+    
+    // Ð´ÈëKruskalËã·¨½á¹û
+    vector<Edge> kruskalResult = kruskal();
+    int kruskalTotalWeight = 0;
+    file << "Kruskal" << endl;
+    for (size_t i = 0; i < kruskalResult.size(); i++) {
+        const Edge& edge = kruskalResult[i];
+        file << edge.from << " " << edge.to << " " << edge.weight << endl;
+        kruskalTotalWeight += edge.weight;
+    }
+    file << "KruskalTotalWeight " << kruskalTotalWeight << endl;
 }
 
 int main() {
-	cout << "Output is in final.txt. Use HTML file to visualize it." <<endl;
-	readComputerInfo("computer.txt");
-	readNetworkInfo("network.txt");
-	writeResult("final.txt");
-	return 0;
+    cout << "Output is in final.txt. Use HTML file to visualize it." <<endl;
+    readComputerInfo("computer.txt");
+    readNetworkInfo("network.txt");
+    writeResult("final.txt");
+    return 0;
 }
+
